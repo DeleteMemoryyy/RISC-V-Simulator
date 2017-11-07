46bea7673c53d7fc46cb55638e0443d00a353587
DeleteMemory <xtliudexin@gmail.com>
2017-11-07 14:22:17 +0800

    update config

--- UI_LIB/config.sh
+++ UI_LIB/config.sh
@@ -3,15 +3,18 @@
 PKG_CONFIG=$(pkg-config --cflags glfw3)
 PKG_EXSITED=-I/usr/local/include
 
-if [ "$PKG_CONFIG" != $PKG_EXSITED ]
+if [ "$PKG_CONFIG" != "$PKG_EXSITED" ]
 then
     echo "  install glfw"
     sudo apt-get build-dep glfw
     sudo apt-get install cmake xorg-dev libglu1-mesa-dev
+    unzip glfw-3.2.1.zip -d glfw-3.2.1
     cd glfw-3.2.1
     sudo cmake .
     sudo make
     sudo make install
+    cd ..
+    rm -r glfw-3.2.1
 else
     echo "  glfw exsited."
 fi
