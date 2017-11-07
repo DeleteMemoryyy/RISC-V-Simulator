97c52b8d5b9830ac32a01b477e4c168ed90959ce
DeleteMemory <xtliudexin@gmail.com>
2017-11-07 14:15:57 +0800

    update config

--- UI_LIB/config.sh
+++ UI_LIB/config.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 PKG_CONFIG=$(pkg-config --cflags glfw3)
-PKG_EXSITED='-I/usr/include/libdrm'
+PKG_EXSITED=-I/usr/local/include
 
-if [ $PKG_CONFIG != $PKG_EXSITED ]
+if [ "$PKG_CONFIG" != $PKG_EXSITED ]
 then
     echo "  install glfw"
     sudo apt-get build-dep glfw
     sudo apt-get install cmake xorg-dev libglu1-mesa-dev
     cd glfw-3.2.1
-    mkdir glfw-build
-    cd glfw-buld
-    sudo cmake ../
+    sudo cmake .
     sudo make
     sudo make install
 else
