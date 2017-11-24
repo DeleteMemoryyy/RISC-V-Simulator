009d91648d5dd18eaec4cbdc9f0f2b1bcab02df1
DeleteMemory <xtliudexin@gmail.com>
2017-11-24 16:24:06 +0800

    add support to different ELF

--- Simulate.cpp
+++ Simulate.cpp
@@ -63,7 +63,7 @@ void setup()
     load_memory();
 
     PC = mainAddr;
-    endPC = mainAddr + mainSize - 3;
+    endPC = ((mainAddr + mainSize - 3) / 16) * 16;
     reg[R_gp] = gp;
     reg[R_sp] = P_TO_V(MEM_ED);
 
